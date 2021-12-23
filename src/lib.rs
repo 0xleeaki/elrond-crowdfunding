7dd5155d29fb9304f96e5c944b578feb40d3b165
lee.aki <lee.aki.924@gmail.com>
2021-12-23 13:42:01 +0700

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![no_std]
+
+use elrond_wasm::imports;
+
+imports!();
+
+#[elrond_wasm_derive::contract(CrowdfundingImpl)]
+pub trait Crowdfunding {
+    #[storage_set("owner")]
+    fn set_owner(&self, address: &Address);
+
+    #[view]
+    #[storage_get("owner")]
+    fn get_owner(&self) -> Address;
+
+    #[init]
+    fn init(&self) {
+        let my_address: Address = self.get_caller();
+        self.set_owner(&my_address);
+    }
+}
